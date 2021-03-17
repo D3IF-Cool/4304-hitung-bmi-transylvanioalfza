5c8eb8db07944fcc69a169b597b12afe5e7158e2
M.Transylvanio_Alfarezha <transylvanioalfar@gmail.com>
2021-03-17 15:42:53 +0700

    Membuat navigasi menggunakan menu

---
+++ app/src/main/java/org/d3if4118/hitungbmi/ui/AboutFragment.kt
@@ -0,0 +1,6 @@
+package org.d3if4118.hitungbmi.ui
+
+import androidx.fragment.app.Fragment
+import org.d3if4118.hitungbmi.R
+
+class AboutFragment : Fragment(R.layout.fragment_about)
--- app/src/main/java/org/d3if4118/hitungbmi/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if4118/hitungbmi/ui/HitungFragment.kt
@@ -2,17 +2,31 @@ package org.d3if4118.hitungbmi.ui
 
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import org.d3if4118.hitungbmi.R
 import org.d3if4118.hitungbmi.data.KategoriBmi
 import org.d3if4118.hitungbmi.databinding.FragmentHitungBinding
 
 class HitungFragment : Fragment() {
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            findNavController().navigate(
+                R.id.action_hitungFragment_to_aboutFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     private lateinit var binding: FragmentHitungBinding
     private lateinit var kategoriBmi: KategoriBmi
@@ -28,6 +42,7 @@ class HitungFragment : Fragment() {
                 actionHitungFragmentToSaranFragment(kategoriBmi)
             )
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
 
