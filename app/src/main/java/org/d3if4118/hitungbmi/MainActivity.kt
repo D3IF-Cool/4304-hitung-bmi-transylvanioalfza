ddc93d3c08b9ddd710b5858ef2f55c32a1194c88
M.Transylvanio_Alfarezha <transylvanioalfar@gmail.com>
2021-03-17 14:05:26 +0700

    Menambahkan navigation component

--- app/src/main/java/org/d3if4118/hitungbmi/MainActivity.kt
+++ app/src/main/java/org/d3if4118/hitungbmi/MainActivity.kt
@@ -2,10 +2,7 @@ package org.d3if4118.hitungbmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.TextUtils
-import android.util.Log
-import android.widget.Toast
-import org.d3if4118.hitungbmi.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
