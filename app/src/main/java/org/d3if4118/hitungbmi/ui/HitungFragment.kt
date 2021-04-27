7164cd0a4a5346dff6da2d8506485af6a01c9ceb
M.Transylvanio_Alfarezha <transylvanioalfar@gmail.com>
2021-04-27 14:02:30 +0700

    Membuat view model untuk fitur hitung

--- app/src/main/java/org/d3if4118/hitungbmi/ui/HitungFragment.kt
+++ app/src/main/java/org/d3if4118/hitungbmi/ui/HitungFragment.kt
@@ -6,6 +6,7 @@ import android.text.TextUtils
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
@@ -15,20 +16,7 @@ import org.d3if4118.hitungbmi.databinding.FragmentHitungBinding
 
 class HitungFragment : Fragment() {
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.options_menu, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.menu_about) {
-            findNavController().navigate(
-                R.id.action_hitungFragment_to_aboutFragment)
-            return true
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
+    private val viewModel: HitungViewModel by viewModels()
     private lateinit var binding: FragmentHitungBinding
     private lateinit var kategoriBmi: KategoriBmi
 
@@ -49,6 +37,32 @@ class HitungFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getHasilBmi().observe(viewLifecycleOwner, {
+            if (it == null) return@observe
+            binding.bmiTextView.text = getString(R.string.bmi_x, it.bmi)
+            binding.kategoriTextView.text = getString(R.string.kategori_x,
+                getKategori(it.kategori))
+            binding.buttonGroup.visibility = View.VISIBLE
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_about) {
+            findNavController().navigate(
+                R.id.action_hitungFragment_to_aboutFragment)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun hitungBmi() {
 
         val berat = binding.beratEditText.text.toString()
@@ -63,7 +77,6 @@ class HitungFragment : Fragment() {
             return
         }
 
-        val tinggiCm = tinggi.toFloat() / 100
         val selectedId = binding.radioGroup.checkedRadioButtonId
         if (selectedId == -1) {
             Toast.makeText(context, R.string.gender_invalid, Toast.LENGTH_LONG).show()
@@ -71,11 +84,17 @@ class HitungFragment : Fragment() {
         }
 
         val isMale = selectedId == R.id.priaRadioButton
-        val bmi = berat.toFloat() / (tinggiCm * tinggiCm)
-        val kategori = getKategori(bmi, isMale)
-        binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
-        binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
-        binding.buttonGroup.visibility = View.VISIBLE
+
+        viewModel.hitungBmi(berat, tinggi, isMale)
+    }
+
+    private fun getKategori(kategori: KategoriBmi): String {
+        val stringRes = when (kategori) {
+            KategoriBmi.KURUS -> R.string.kurus
+            KategoriBmi.IDEAL -> R.string.ideal
+            KategoriBmi.GEMUK -> R.string.gemuk
+        }
+        return getString(stringRes)
     }
 
     private fun shareData() {
@@ -98,27 +117,4 @@ class HitungFragment : Fragment() {
             startActivity(shareIntent)
         }
     }
-
-    private fun getKategori(bmi: Float, isMale: Boolean): String {
-        kategoriBmi = if (isMale) {
-            when {
-                bmi < 20.5 -> KategoriBmi.KURUS
-                bmi >= 27.0 -> KategoriBmi.GEMUK
-                else -> KategoriBmi.IDEAL
-            }
-        } else {
-            when {
-                bmi < 18.5 -> KategoriBmi.KURUS
-                bmi >= 25.0 -> KategoriBmi.GEMUK
-                else -> KategoriBmi.IDEAL
-            }
-        }
-        val stringRes = when (kategoriBmi) {
-            KategoriBmi.KURUS -> R.string.kurus
-            KategoriBmi.IDEAL -> R.string.ideal
-            KategoriBmi.GEMUK -> R.string.gemuk
-        }
-        return getString(stringRes)
-    }
-
 }
